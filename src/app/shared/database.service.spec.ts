5094a9017f34f5b09c4d2b40c0cadbc62fd90155
Akhilesh <akhilesh.9234@gmail.com>
2023-11-09 17:40:43 +0530

    spec.ts

--- src/app/shared/database.service.spec.ts
+++ src/app/shared/database.service.spec.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from '../app-routing.module';
 import { AngularFireModule } from '@angular/fire/compat';
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import {HttpClientTestingModule} from '@angular/common/http/testing'
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 
 describe('DatabaseService', () => {
   let service: DatabaseService;
+  let httpController : HttpTestingController;
 
   beforeEach(() => {
     
@@ -20,12 +21,25 @@ describe('DatabaseService', () => {
     ]
     });
     service = TestBed.inject(DatabaseService);
+    httpController = TestBed.inject(HttpTestingController)
 
   });
 
   it(' service should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+
+  it("should get all CArs", () => {
+    const mockdata:any = []
+    service.getCarsData().subscribe((cars: any) => {
+        expect(cars).toEqual(mockdata)
+    });
+    
+
+   
+  })
 
   
 });
